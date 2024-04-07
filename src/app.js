2a06113c8d0b31245fda0eba44f75b88627311ce
Mohd Saqib <mohdsaqib4442@gmail.com>
2024-04-08 03:48:04 +0530

    added error middleware

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials:true
 }))
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json({limit:"16kb"}));
 app.use(express.static("public"));
@@ -24,6 +25,7 @@ import healthcheckRouter from "./routes/healthcheck.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js"
 import likeRouter from "./routes/like.routes.js"
 import dashboardRouter from "./routes/dashboard.routes.js"
+import { ApiResponse } from "./utils/ApiResponse.js";
 
 app.use("/api/v1/healthcheck", healthcheckRouter);
 app.use("/api/v1/users", userRouter);
@@ -33,7 +35,21 @@ app.use("/api/v1/tweets", tweetRouter);
 app.use("/api/v1/playlists", playlistRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
 app.use("/api/v1/likes", likeRouter);
-app.use("/api/v1/dashboard",dashboardRouter)
+app.use("/api/v1/dashboard", dashboardRouter)
+
+
+// error handler
+app.use((err, req, res, next) => {
+  console.log("stack error ::", err.stack);
+    return res.status(err.statusCode || 500).json(
+        new ApiResponse(
+            err.statusCode || 500,
+            null,
+            err.message || "Error From Server"
+        )
+    );
+});
+
 
 
 
